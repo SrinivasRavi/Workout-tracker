a09bcddd301b4bd2695901d104be71450b2959f5
Srinivas Ravi <srinivas.ravi.official@gmail.com>
2024-08-10 23:03:55 -0700

    Initial Commit

---
+++ Workout tracker/Workout_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_trackerApp.swift
+//  Workout tracker
+//
+//  Created by Srinivas Ravi on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Workout_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
